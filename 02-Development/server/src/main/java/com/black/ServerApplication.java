a2e90a01614d9159395090c6c7177729a81a08a5
Black <black524726@163.com>
2021-12-06 22:01:26 +0800

    init swagger ui test RestfulAPTI

--- 02-Development/server/src/main/java/com/black/ServerApplication.java
+++ 02-Development/server/src/main/java/com/black/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.black.server;
+package com.black;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- 02-Development/server/src/main/java/com/black/controller/HelloWorldController.java
+++ 02-Development/server/src/main/java/com/black/controller/HelloWorldController.java
@@ -1,14 +1,19 @@
 package com.black.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * @author Black
  */
+@Api(value = "desc of class")
 @RestController
 public class HelloWorldController {
-    @RequestMapping("/hello")
+    @ApiOperation(value = "desc of method")
+    @PostMapping("/hello")
     public String say(){
         return "Hello World";
     }
